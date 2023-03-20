6af894f5791f2cc22dcd14e1a0c1df6396b9cf56
Dalton Walls <scallywig@gmail.com>
2023-03-20 17:29:41 -0400

    edits

--- CarInsuranceQuoteApp/CarInsuranceQuoteApp/Program.cs
+++ CarInsuranceQuoteApp/CarInsuranceQuoteApp/Program.cs
@@ -16,28 +16,9 @@ namespace CarInsuranceQuoteApp
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
 
-            //if (age > 15)
-            //{
-            //    Console.WriteLine("You are of legal age.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You must be at least 15+ to qualify.");
-            //}
-
             Console.WriteLine("Have you ever had a DUI?");
             bool DUI = Convert.ToBoolean(Console.ReadLine());
-
-            //if (Yes != true)
-                
-            //{
-            //    Console.WriteLine("You cannot have any DUIs on recored.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You have qualified");
-            //}
-
+            
             Console.WriteLine("How many speeding tickets do you have?");
             int sum = Convert.ToInt32(Console.ReadLine());
 
