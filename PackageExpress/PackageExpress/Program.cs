73cebfdefff59b36a4bbe2ceac7e63df99d628a4
Dalton Walls <scallywig@gmail.com>
2023-03-21 23:08:52 -0400

    edit to add comments

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -6,12 +6,16 @@ namespace PackageExpress
     {
         static void Main(string[] args)
         {
+            // The WriteLine() writes the specified data, followed by the current line terminator, to the standard output stream.
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
 
             Console.WriteLine("What is your package weight?");
+            // Covert.ToInt32 turns a string in to an integer.
             int packWeight = Convert.ToInt32(Console.ReadLine());
+            // If statement returning back data that determines if weight is heavier or lighter than 50 lbs.
             if (packWeight > 50)
             {
+                // Terminates this process and returns an exit code to the operating system.
                 System.Environment.Exit(1);
             }
             else
@@ -27,7 +31,7 @@ namespace PackageExpress
 
             Console.WriteLine("What is your package length?");
             int packLength = Convert.ToInt32(Console.ReadLine());
-
+            // Adding variables total 
             if (packWidth + packHeight + packLength > 50)
             {
                 Console.WriteLine("Package too big to be shipped vie Package Express.");
@@ -37,7 +41,8 @@ namespace PackageExpress
             {
                 Console.WriteLine("Package weight qualifies for delivery, proceed.");
             }
-
+            Console.ReadLine();
+            // Variable using multiplication and divide operators.
             int dimensions = packLength * packHeight * packWidth;
             int stamped = dimensions * packWeight;
             int total = stamped / 100;
