39e543afa3feb955419edd08bbb7d1463411ef5b
Dalton Walls <scallywig@gmail.com>
2023-03-21 21:30:27 -0400

    push

--- StaffSalaryApp/StaffSalaryApp/Program.cs
+++ StaffSalaryApp/StaffSalaryApp/Program.cs
@@ -1,32 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace StaffSalaryApp
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             Console.WriteLine("Anonymous Income Comparison Program");
 
             Console.WriteLine("Person 1");
-            decimal HourlyRate = 24.75m;
-            int HoursWorked = 40;
-            decimal Person1 = HourlyRate * HoursWorked;
-            Console.WriteLine("Annual salary of Person 1: " + Person1);
+            decimal HourlyRate = Convert.ToDecimal(Console.ReadLine());
+            int HoursWorked = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Person 2");
-            decimal HourlyRate2 = 29.50m;
-            int HoursWorked2 = 35;
-            decimal Person2 = HourlyRate2 * HoursWorked2;
-            Console.WriteLine("Annual salary of Person 2: " + Person2);
+            decimal HourlyRate2 = Convert.ToDecimal(Console.ReadLine());
+            int HoursWorked2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Annual salary of Person 1: ");
+            decimal one = HourlyRate * HoursWorked * 52;
+            Console.WriteLine(one);
+
+            Console.WriteLine("Annual salary of Person 2: ");
+            decimal two = HourlyRate2 * HoursWorked2 * 52;
+            Console.WriteLine(two);
 
             Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(one > two);
 
-            bool Per1OrPer2 = Person1 > Person2;
-            Console.WriteLine(Per1OrPer2);
-            Console.ReadLine();
-
-
+            Console.ReadKey();
         }
     }
 }
