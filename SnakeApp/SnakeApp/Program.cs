a1de92eb2073b7d7af4fd3480491fe78945899b7
Dalton Walls <scallywig@gmail.com>
2023-03-22 18:57:21 -0400

    push for references

---
+++ SnakeApp/SnakeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the most venomous snake in the world?");
+        }
+    }
+}
