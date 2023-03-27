c79ac00b85ecea3557c852fd2d542528a881f302
Dalton Walls <scallywig@gmail.com>
2023-03-27 19:43:56 -0400

    push

--- ArrayExample/ArrayExample/Program.cs
+++ ArrayExample/ArrayExample/Program.cs
@@ -15,107 +15,139 @@ using System.Linq;
             // In C#, if statement is used to indicate which statement will execute according to the value of the given boolean expression
             if (type == "dogs")
             {
-            Console.WriteLine("My favorite type of animal are " + type);
+                Console.WriteLine("My favorite type of animal are " + type);
             }
         // The for statement executes a statement or a block of statements while a specified Boolean expression evaluates to true. 
         for (int a = 0; a < animals.Length; a++)
-            {
-             Console.WriteLine(animals[a]);
-            }
+        {
+            Console.WriteLine(animals[a]);
+        }
         // This is a loop that results in a loop using the leass than(<) sign.
         while (animals.Length < 6)
-            {
+        {
             Console.WriteLine("To infininty and beyond!");
             break;
-            }
+        }
         // This is a loop that results in a loop using the leass than and equal(<=) sign.
         while (animals.Length <= 6)
-            {
+        {
             Console.WriteLine("Is it safe not to loop infinitely anymore?");
             break;
-            }
+        }
         // Reads the next line of characters from the standard input stream.
         Console.ReadLine();
 
-        //Console.WriteLine("What show does Andrew Lincoln start in?");
-        //string shows = Convert.ToString(Console.ReadLine());
-        //bool correct = shows == "TWD";
-
-        //    switch (shows)
-        //    {
-        //        case "Invincible":
-        //            Console.WriteLine("Even thogh it's possible, I doubt he had time to do voice acting..");
-        //            Console.WriteLine("Guess again?");
-        //            shows = Convert.ToString(Console.ReadLine());
-        //            break;
-        //        case "Game of Thrones":
-        //            Console.WriteLine("He never even made a guess appearance in that show. Odd choice.");
-        //            Console.WriteLine("Guess again?");
-        //            shows = Convert.ToString(Console.ReadLine());
-        //            break;
-        //        case "TWD":
-        //            Console.WriteLine("That is right. He was the main character of the show. Great guess!");
-        //        correct = true;
-        //            break;
-        //        case "Seinfield":
-        //            Console.WriteLine("Really? In what world did that happen?");
-        //            Console.WriteLine("Guess again?");
-        //            shows = Convert.ToString(Console.ReadLine());
-        //            break;
-        //        case "default":
-        //            Console.WriteLine("Not even close!");
-        //            Console.WriteLine("Guess again?");
-        //            shows = Convert.ToString(Console.ReadLine());
-        //            break;
-        //        case "Friends":
-        //            Console.WriteLine("Andrew has the gift of laughter now? Doubt it.");
-        //            Console.WriteLine("Guess again?");
-        //            shows = Convert.ToString(Console.ReadLine());
-        //            break;
-        //    }
-        //while (!correct);
-        //Console.ReadLine();
-
-
-
-        List<string> myTeams = new List<string>
-            {
-                "Lakers", "Dolphins", "Mariners", "Kings", "Seminoles", "Brazil", "Dolphins"
-            };
-        List<string> snubs = new List<string>();
-        myTeams.Add("Dodgers");
-        myTeams.Add("Heat");
-        myTeams.Add("Lakers");
-        Console.WriteLine("Choose a team");
-        string select = Console.ReadLine();
-
-        if (myTeams.Contains(select))
+        List<string> fruits = new List<string>
         {
-            for (int i = 0; i < myTeams.Count(); i++)
+            "banana", "apple", "plum", "orange", "grape"
+        };
+        Console.WriteLine("Pick a fruit");
+        string pick = Console.ReadLine();
+        foreach (string fruit in fruits)
+        {
+            if (fruits.Contains(pick))
             {
-                if (myTeams[i] == select)
-                {
-                    Console.WriteLine(i);
-                }
-                foreach (string team in myTeams)
-                {
-                    //If the item does not exist, add it to "snubs"
-                    if (!snubs.Contains(team))
-                    {
-                        snubs.AddRange(myTeams);
-                    }
-                }
+                Console.WriteLine("That fruit is in the list");
+            }
+            else
+            {
+                Console.WriteLine("That fruit is not in the list");
+                break;
             }
         }
-        else
+        Console.ReadLine();
+
+
+        List<string> desserts = new List<string>
         {
-            Console.WriteLine("Team not in list");
-        }
-        //Display items in the list
-        foreach (string loss in snubs)
+            "shortbread", "cookies", "icecream", "pumpkinpie", "cake", "brownies", "cake", "danish"
+        };
+        Console.WriteLine("What dessert do you prefer the most");
+        string sweets = Console.ReadLine();
+        foreach (string sweet in desserts)
         {
-            Console.WriteLine("A fan of the {0}", loss);
+            if (desserts.Contains(sweets))
+            {
+                Console.WriteLine(sweets + " is is in the list");
+                break;
+            }
+            else
+            {
+                Console.WriteLine(sweets + " is not in the list");
+                break;
+            }
         }
+        for (int i = 0; i < desserts.Count(); i++)
+        {
+            Console.WriteLine(desserts[i]);
+        }
+        Console.ReadLine();
+
+        // Represents a strongly typed list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists.
+        List<string> myTeams = new List<string>
+            {
+                "Lakers", "Dolphins", "Mariners", "Kings", "Seminoles", "Brazil", "Dolphins", "Lakers"
+            };
+        List<string> snubs = new List<string>();
+        Console.WriteLine("Choose a team");
+        string select = Console.ReadLine();
+        // The foreach is creating a local string in the variable list myTeams.
+        foreach (string team in myTeams)
+        {
+            // C# if Statement The if statement contains a boolean condition followed by a single or multi-line code block to be executed.
+            if (snubs.Contains(team))
+            {
+                Console.WriteLine(team + " team is a duplicate in list ");
+            }
+            else // An else statement is used with if statement to execute some block of code if the given condition is false.
+            {
+                Console.WriteLine(team + " team is not a duplicate");
+                // Add strings together, appending them, with the plus operator.
+                snubs.Add(team);
+            }
+        }
+
+        //if (myTeams.Contains(select))
+        //{
+        //    for (int i = 0; i < myTeams.Count(); i++)
+        //    {
+        //        if (myTeams[i] == select)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //        foreach (string team in myTeams)
+        //        {
+        //            //If the item does not exist, add it to "snubs"
+        //            if (!snubs.Contains(team))
+        //            {
+        //                snubs.AddRange(myTeams);
+        //            }
+        //        }
+        //        foreach (myTeams)
+        //        {
+        //            if(team != select)
+        //            {
+        //                Console.WriteLine("This team is appears multiple times");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("This team appears once");
+        //            }
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Team not in list");
+        //}
+        ////Display items in the list
+        //foreach (string loss in snubs)
+        //{
+        //    Console.WriteLine("A fan of the {0}", loss);
+        //}
+
+
+
         Console.ReadLine();
     } 
     }
