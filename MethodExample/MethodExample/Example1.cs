2e45bf8ed1326ffb04b24ff392cef8d07305cfb0
Dalton Walls <scallywig@gmail.com>
2023-04-02 00:00:34 -0400

    push

---
+++ MethodExample/MethodExample/Example1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodExample
+{
+    class Example
+    {
+        public int Born;
+        public int Age;
+        public double Date;
+
+        public Example (int argBorn, int argAge, int argDate)
+        {
+            Born = argBorn;
+            Age = argAge;
+            Date = argDate;
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ MethodExample/MethodExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Example example = new Example ();
+            Example example1 = new Example ();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
