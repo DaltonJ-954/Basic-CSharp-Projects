250b0611df707ad7e1b1ca09e453dd02f413edb9
Dalton Walls <scallywig@gmail.com>
2023-04-05 16:34:57 -0400

    push to online

--- MethodVoid/MethodVoid/ExampleVoid.cs
+++ MethodVoid/MethodVoid/ExampleVoid.cs
@@ -6,10 +6,50 @@ namespace MethodVoid
 {
     class ExampleVoid
     {
-        int num1 = 2;
-        public void myMethod()
+        public void intMethod() // A class method
         {
-            Console.WriteLine(num1) ;
+            int num1 = 43; // A variable being declared as an integer
+            Console.WriteLine(num1 / 2); // Console WriteLine outputting the operation value
+        }
+
+        public void Addition(out int box) // Method with output parameters
+        {
+            box = 30;
+            box += box;
         }
     }
+
+    static class OverLoad // This class is used to demonstrate the overload method as well as declaring it to be static.
+    {
+        public void overLoadInt(int sum1) // The method passes off as a integer parameter
+        {
+            return;
+        }
+
+        public void overLoadStr(string okay) // The method passes off as a string parameter
+        {
+            return;
+        }
+
+        public void overLoadCombo(int sum2, string okay) // The method passes off an integer and string parameter
+        {
+            return; // In C#, a method can return any type of data including objects. In other words, methods are allowed to return objects without any compile time error.
+        }
+    }
+
+    //static class Person
+    //{
+    //    // Fields
+    //    private string name;
+    //    private int age;
+
+    //    // Constructor
+    //    public Person(string name, int age)
+    //    {
+    //        this.name = name;
+    //        this.age = age;
+    //    }
+    //}
+
 }
+
--- MethodVoid/MethodVoid/Program.cs
+++ MethodVoid/MethodVoid/Program.cs
@@ -6,11 +6,31 @@ namespace MethodVoid
     {
         static void Main(string[] args)
         {
-            ExampleVoid example = new ExampleVoid();
-            Console.WriteLine("Please enter a number: ");
-            int enter = Convert.ToInt32(Console.ReadLine());
+            ExampleVoid intNum = new ExampleVoid();
+            intNum.intMethod();
+
+
+            try
+            {
+                Console.WriteLine("Please enter a number: ");
+                int split = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(split / 2);
+            }
+            catch
+            {
+                return;
+            }
+            Console.ReadKey();
+
+            OverLoad loader = new OverLoad();
+
+            loader.overLoadInt(34);
+            loader.overLoadStr("Tech");
+            loader.overLoadCombo(2023, "Academy");
 
             Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
+
