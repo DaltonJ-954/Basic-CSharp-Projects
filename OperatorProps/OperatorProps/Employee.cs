1eeff6b81f915da4ffe3e5adf473f26221659ad2
Dalton Walls <scallywig@gmail.com>
2023-04-07 19:48:08 -0400

    push assignment

---
+++ OperatorProps/OperatorProps/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorProps
+{
+    class Employee
+    {
+        // Three Properties in the Employee class
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee (string FirstName, int ID) // Constructor pulling two variables in it's parameter from the Property field
+        {
+            this.FirstName = FirstName;
+            this.ID = ID;
+        }
+        public Employee (int ID, string FirstName, string LastName) // Constructor that has all three variables in it's parameter
+        {
+            this.ID = ID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+}
---
+++ OperatorProps/OperatorProps/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(33498, "Dalton", "Walls");
+            Employee employee10 = new Employee(97204, "Tech", "Academy");
+
+            if (employee.FirstName == employee10.FirstName)
+            {
+                Console.WriteLine("These two strings have the same value");
+            }
+            else
+            {
+                Console.WriteLine("These two strings are not of the same value");
+            }
+            Console.ReadLine();
+        }
+    }
+}
