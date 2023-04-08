6474e100f3e0cf9fbf364d8a394a7bdf92410d08
Dalton Walls <scallywig@gmail.com>
2023-04-08 16:14:51 -0400

    push edit

---
+++ OpsOverride/OpsOverride/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace OperatorProps
+{
+    class Employee
+    {
+        // Three Properties in the Employee class.
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee2) // Constructor pulling two variables in it's parameter from the Property field.
+        {
+            if (employee.ID.Equals(employee2.ID)) // Comparing both ID values in the two instances to be true.
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Employee employee, Employee employee2) // Constructor pulling two variables in it's parameter from the Property field
+        {
+            if (!employee.ID.Equals(employee2.ID)) // Comparing both ID values in the two instances to not  be true.
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //public bool Equals (Employee other)
+        //{
+        //    return other.ID.Equals(this.ID);
+        //}
+
+        ////public override bool Equals (object obj)
+        ////{
+        ////    Employee employee = obj as Employee;
+        ////    if (employee != null)
+        ////    {
+        ////        return employee.ID.Equals(this.ID);
+        ////    }
+        ////    return false;
+        ////}
+    }
+}
