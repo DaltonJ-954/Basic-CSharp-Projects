89dfa142246e76c7edc5ca581265fb1a388bdfac
Dalton Walls <scallywig@gmail.com>
2023-04-24 13:34:04 -0400

    update

---
+++ MVC101/MVC101/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using MVC101.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC101.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Marion";
+            user.LastName = "Oddly";
+            user.Age = 25;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid Page");
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=10)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplicationTutorial/WebApplicationTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTutorial.Global" Language="C#" %>
