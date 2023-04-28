6c5b8902bd074445d62c83e528eb07c76bc5002f
Dalton Walls <scallywig@gmail.com>
2023-04-27 21:07:19 -0400

    update

---
+++ StudentInfo/StudentInfo/Data/StudentInfoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentInfo.Models;
+
+namespace StudentInfo.Data
+{
+    public class StudentInfoContext : DbContext
+    {
+        public StudentInfoContext (DbContextOptions<StudentInfoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentInfo.Models.Student> Student { get; set; }
+    }
+}
