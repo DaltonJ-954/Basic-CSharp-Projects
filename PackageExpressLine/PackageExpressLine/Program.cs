2bda5d8ca734d2f60fc3934dd6553bd0957ef84b
Dalton Walls <scallywig@gmail.com>
2023-10-05 19:24:36 -0400

    Created/Delete
    Had to redo a program over again and delete the old one

---
+++ PackageExpressLine/PackageExpressLine/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The WriteLine() writes the specified data, followed by the current line terminator, to the standard output stream.
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("What is your package weight?");
+            // Covert.ToInt32 turns a string in to an integer.
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            // If statement returning back data that determines if weight is heavier or lighter than 50 lbs.
+            if (packWeight > 75)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day!");
+                Console.ReadKey();
+                // Terminates this process and returns an exit code to the operating system.
+                System.Environment.Exit(1);
+            }
+            else
+            {
+                Console.WriteLine("Package weight qualifies for delivery, proceed.");
+            }
+
+            Console.WriteLine("What is your package width?");
+            int packWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your package height?");
+            int packHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your package length?");
+            int packLength = Convert.ToInt32(Console.ReadLine());
+            // Adding variables total 
+            if (packWidth + packHeight + packLength > 65)
+            {
+                Console.WriteLine("Package too big to be shipped vie Package Express.");
+                Console.ReadKey();
+                System.Environment.Exit(1);
+            }
+            else
+            {
+                Console.WriteLine("Package weight qualifies for delivery, proceed.");
+            }
+            Console.ReadLine();
+            // Variable using multiplication and divide operators.
+            int dimensions = packLength * packHeight * packWidth;
+            int stamped = dimensions * packWeight;
+            int total = stamped / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: $" + total + "\nThank You!");
+
+            Console.ReadLine();
+        }
+    }
+}
