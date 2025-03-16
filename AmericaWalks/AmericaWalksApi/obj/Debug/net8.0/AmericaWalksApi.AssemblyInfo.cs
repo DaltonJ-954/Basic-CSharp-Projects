40a76e08f1948b53fd413cfcdb5bc5134e395405
Dalton Walls <scallywig@gmail.com>
2025-03-16 11:55:25 -0400

    edits

--- AmericaWalks/AmericaWalksApi/obj/Debug/net8.0/AmericaWalksApi.AssemblyInfo.cs
+++ AmericaWalks/AmericaWalksApi/obj/Debug/net8.0/AmericaWalksApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmericaWalksApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9827ff699984085bf845bfac754eeab4d51be93d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c678b62ed807d2cfa9bfee3e9142624a47a8f022")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmericaWalksApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmericaWalksApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
